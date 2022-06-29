22ee4e9f3e73e113ca0a2d169e7723050cd0d125
Luis Miguel Báez Aponte <es.luismiguelbaez@gmail.com>
2022-06-28 22:59:50 -0500

    config file was updated

--- src/config.js
+++ src/config.js
@@ -1,2 +1,10 @@
-export const HOST_BACKEND = process.env.HOST_BACKEND || "http://localhost:3000";
-export const POST = process.env.PORT || 3001;
+const HOST_BACKEND = process.env.HOST_BACKEND || "http://localhost:3000";
+const PORT = process.env.PORT || 3001;
+
+console.log(HOST_BACKEND);
+console.log(PORT);
+
+export default {
+    HOST_BACKEND,
+    PORT
+};
