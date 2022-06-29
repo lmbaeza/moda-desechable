615645525c8a520354f588e49c90b41b9a2cd25a
Luis Miguel Báez Aponte <es.luismiguelbaez@gmail.com>
2022-06-28 23:30:16 -0500

    api backend was added

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
-export const HOST_BACKEND = process.env.HOST_BACKEND || "http://localhost:3000";
+export const HOST_BACKEND = process.env.HOST_BACKEND || "https://sir-tex-api.herokuapp.com" || "http://localhost:3000";
 export const PORT = process.env.PORT || 3001;
